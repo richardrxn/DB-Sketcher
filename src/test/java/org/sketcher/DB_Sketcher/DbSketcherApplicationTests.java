37b0fd95af61ab7f15f08366966fb973dd0c0e47
rullebrett <74204667+rullebrett@users.noreply.github.com>
2025-04-01 20:37:30 -0400

    springboot initializer

---
+++ src/test/java/org/sketcher/DB_Sketcher/DbSketcherApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sketcher.DB_Sketcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbSketcherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

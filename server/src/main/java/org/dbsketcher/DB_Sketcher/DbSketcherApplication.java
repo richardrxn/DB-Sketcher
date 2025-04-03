993b7d7c6175084608dcbadaf6ec017974f1370b
rullebrett <74204667+rullebrett@users.noreply.github.com>
2025-04-03 10:19:21 -0400

    subdividing modules

---
+++ server/src/main/java/org/dbsketcher/DB_Sketcher/DbSketcherApplication.java
@@ -0,0 +1,13 @@
+package org.dbsketcher.DB_Sketcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbSketcherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbSketcherApplication.class, args);
+	}
+
+}
